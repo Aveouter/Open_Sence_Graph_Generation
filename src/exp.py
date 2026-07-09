1f332bdaeca6197b6f9be9cb78e909eb52a1a7ad
abinyzl <1397347762@qq.com>
2026-07-09 22:19:57 +0800

    fix(ci): drop C4 lint rule, fix import order in exp.py

--- src/exp.py
+++ src/exp.py
@@ -1,32 +1,32 @@
 import os
+import os.path as osp
 import sys
 import time
-import os.path as osp
 from datetime import datetime
 
+import lightning.pytorch.callbacks as lc
 import torch
 from fvcore.nn import FlopCountAnalysis, flop_count_table
-from lightning import seed_everything, Trainer
+from lightning import Trainer, seed_everything
 from lightning.pytorch.loggers import CSVLogger
 from lightning.pytorch.profilers import AdvancedProfiler
-import lightning.pytorch.callbacks as lc
 
-from src.methods import method_maps
 from data.dataloaders.base_data import BaseDataModule
+from src.methods import method_maps
 from utils import (
+    BestCheckpointCallback,
+    EpochEndCallback,
+    SetupCallback,
     get_dataset,
     measure_throughput,
-    SetupCallback,
-    EpochEndCallback,
-    BestCheckpointCallback,
 )
 from utils.path_utils import (
-    resolve_output_paths,
-    normalize_ex_name,
-    ensure_unique_run_dir,
     collect_metadata,
-    write_metadata,
+    ensure_unique_run_dir,
+    normalize_ex_name,
+    resolve_output_paths,
     save_eval_results,
+    write_metadata,
 )
 
 
